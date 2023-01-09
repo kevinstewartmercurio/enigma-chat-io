3f1d19c8be19f1bbb278629b1b5c768e57309ffd
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-01-09 17:22:39 -0500

    updating config.js export

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import pkg from "../package.json";
 
-export const def = {
+const def = {
   development: {
     endpoint: pkg.proxy
   },
@@ -8,3 +8,5 @@ export const def = {
     endpoint: window.location.hostname
   }
 }
+
+export default def;
